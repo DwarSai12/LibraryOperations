34e4e5e1942619c936ec83f3f6e78839cb395ab9
sai <sai.dhawanjewar@gmail.com>
2025-05-25 01:12:28 +0100

    New functionality added

--- src/main/java/com/test/Payload.java
+++ src/main/java/com/test/Payload.java
@@ -1,90 +1,45 @@
 package com.test;
 
 public class Payload {
+
+	// Add place API
 	public static String addPlace() {
-		return "{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Frontline house\",\r\n"
+		return "{\r\n" + "  \"location\": {\r\n" + "    \"lat\": -38.383494,\r\n" + "    \"lng\": 33.427362\r\n"
+				+ "  },\r\n" + "  \"accuracy\": 50,\r\n" + "  \"name\": \"Frontline house\",\r\n"
 				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}";
+				+ "  \"address\": \"29, side layout, cohen 09\",\r\n" + "  \"types\": [\r\n" + "    \"shoe park\",\r\n"
+				+ "    \"shop\"\r\n" + "  ],\r\n" + "  \"website\": \"http://google.com\",\r\n"
+				+ "  \"language\": \"French-IN\"\r\n" + "}";
 	}
 
-	
+	// Book price API -- LibraryOperations
 	public static String bookPrice() {
-		return "{\r\n"
-				+ "\r\n"
-				+ "\"dashboard\": {\r\n"
-				+ "\r\n"
-				+ "\"purchaseAmount\": 910,\r\n"
-				+ "\r\n"
-				+ "\"website\": \"rahulshettyacademy.com\"\r\n"
-				+ "\r\n"
-				+ "},\r\n"
-				+ "\r\n"
-				+ "\"courses\": [\r\n"
-				+ "\r\n"
-				+ "{\r\n"
-				+ "\r\n"
-				+ "\"title\": \"Selenium Python\",\r\n"
-				+ "\r\n"
-				+ "\"price\": 50,\r\n"
-				+ "\r\n"
-				+ "\"copies\": 6\r\n"
-				+ "\r\n"
-				+ "},\r\n"
-				+ "\r\n"
-				+ "{\r\n"
-				+ "\r\n"
-				+ "\"title\": \"Cypress\",\r\n"
-				+ "\r\n"
-				+ "\"price\": 40,\r\n"
-				+ "\r\n"
-				+ "\"copies\": 4\r\n"
-				+ "\r\n"
-				+ "},\r\n"
-				+ "\r\n"
-				+ "{\r\n"
-				+ "\r\n"
-				+ "\"title\": \"RPA\",\r\n"
-				+ "\r\n"
-				+ "\"price\": 45,\r\n"
-				+ "\r\n"
-				+ "\"copies\": 10\r\n"
-				+ "\r\n"
-				+ "}\r\n"
-				+ "\r\n"
-				+ "]\r\n"
-				+ "\r\n"
-				+ "}";
-	} 
-	
+		return "{\r\n" + "\r\n" + "\"dashboard\": {\r\n" + "\r\n" + "\"purchaseAmount\": 910,\r\n" + "\r\n"
+				+ "\"website\": \"rahulshettyacademy.com\"\r\n" + "\r\n" + "},\r\n" + "\r\n" + "\"courses\": [\r\n"
+				+ "\r\n" + "{\r\n" + "\r\n" + "\"title\": \"Selenium Python\",\r\n" + "\r\n" + "\"price\": 50,\r\n"
+				+ "\r\n" + "\"copies\": 6\r\n" + "\r\n" + "},\r\n" + "\r\n" + "{\r\n" + "\r\n"
+				+ "\"title\": \"Cypress\",\r\n" + "\r\n" + "\"price\": 40,\r\n" + "\r\n" + "\"copies\": 4\r\n" + "\r\n"
+				+ "},\r\n" + "\r\n" + "{\r\n" + "\r\n" + "\"title\": \"RPA\",\r\n" + "\r\n" + "\"price\": 45,\r\n"
+				+ "\r\n" + "\"copies\": 10\r\n" + "\r\n" + "}\r\n" + "\r\n" + "]\r\n" + "\r\n" + "}";
+	}
+
+	// Add book API -- LibraryOperation
 	public static String addBook(String isbn, int aisle) {
-		String addbook = "{\r\n"
-				+ "\"name\":\"Learn Appium Automation with Java\",\r\n"
-				+ "\"isbn\":\""+isbn+"\",\r\n"
-				+ "\"aisle\":\""+aisle+"\",\r\n"
-				+ "\"author\":\"John foer\"\r\n"
-				+ "}";
-		
+		String addbook = "{\r\n" + "\"name\":\"Learn Appium Automation with Java\",\r\n" + "\"isbn\":\"" + isbn
+				+ "\",\r\n" + "\"aisle\":\"" + aisle + "\",\r\n" + "\"author\":\"John foer\"\r\n" + "}";
+
 		return addbook;
 	}
 
-
-public static String deleteBook(String ID) {
-	String deletebook="{\r\n"
-			+ "    \"ID\": \""+ID+"\"\r\n"
-			+ "}";
-	return deletebook;
-}
+	// Delete Book API - LibraryOperation
+	public static String deleteBook(String ID) {
+		String deletebook = "{\r\n" + "    \"ID\": \"" + ID + "\"\r\n" + "}";
+		return deletebook;
+	}
+	
+	public static void sampleMethod() {
+		System.out.println("Trial");
+	}
+	
+	//code updated for git demo
 }
